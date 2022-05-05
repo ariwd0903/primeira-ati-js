73f7071c0b7c198f38d6c66039ddd6ebfadcdd7e
lari <102737242+ariwd0903@users.noreply.github.com>
2022-05-05 09:02:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('Olá, mundo!');
+console.log(7+4);
+console.log('7'+'4')
+console.log(10 + 20 * 30)
+console.log(4**2/30)
+console.log((9**4+2)*6-1)
+console.log(10 % 3* 10** 1 + 1 - 10 * 4 / 2)
+console.log('Larissa')
+const x= 3;
+const y= 5;
+console.log(2*x*3*y);
+const n1= 3;
+const n2= 5;
+const n3= 7;
+console.log(2+5+7);
+const salario= 1800
+console.log (salario*2)
