c7e088432379c323574d4541f266af345caeab96
lari <102737242+ariwd0903@users.noreply.github.com>
2022-05-05 10:51:26 -0300

    Add files via upload

---
+++ at2.js
@@ -0,0 +1,29 @@
+function meunome (NOME){
+    console.log (NOME)
+    return 
+}
+
+meunome('Larissa linda');
+
+function soma (a,b){
+    console.log (a+b)
+    return
+}
+
+soma (9,3);
+
+function multiplicação (c,d){
+    console.log (c*d)
+    return
+}
+
+multiplicação (9,3);
+
+function media (e,f,g,h){
+    let res= (e+f+g+h)/4
+    console.log(res)
+    return
+}
+
+media (9,3,2,3);
+
